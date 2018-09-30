72f144515d1576621e613a4b916573550fb52506
AMahno <cropemail@gmail.com>
2018-09-30 22:14:43 +0300

    Lab1 initial

---
+++ lab_1/regFile.v
@@ -0,0 +1,26 @@
+module regFile(i_clk, 
+               i_raddr1, 
+               i_raddr2, 
+               i_waddr, 
+               i_wdata, 
+               i_we,
+               o_rdata1,
+               o_rdata2 
+               );
+               
+	input           i_clk, i_we;
+	input   [4:0]   i_raddr1, i_raddr2, i_waddr;
+	input   [31:0]  i_wdata;           
+	output reg [31:0]  o_rdata1, o_rdata2;
+
+	reg [31:0] regs[4:0];
+	
+	always @(posedge i_clk)
+		if(i_we)
+			regs[i_waddr][31:0] <= i_wdata[31:0];
+		else begin
+			o_rdata1 <= regs[i_raddr1];
+			o_rdata2 <= regs[i_raddr2];
+		end
+  
+endmodule
