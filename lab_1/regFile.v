fbc72107e7556ddb1f09556782caeaf8fb89d9df
AMahno <cropemail@gmail.com>
2018-10-09 11:43:38 +0300

    Correct reg file for lab1

--- lab_1/regFile.v
+++ lab_1/regFile.v
@@ -11,20 +11,16 @@ module regFile(i_clk,
 	input           i_clk, i_we;
 	input   [4:0]   i_raddr1, i_raddr2, i_waddr;
 	input   [31:0]  i_wdata;           
-	output reg [31:0]  o_rdata1, o_rdata2;
+	output [31:0]  o_rdata1, o_rdata2;
 
 	reg [31:0] regs[31:0];
 	
 	always @(posedge i_clk) begin
-		if(i_we) begin
+		if(i_we)
 			regs[i_waddr] <= i_wdata;
-		end
-
-		if(i_raddr1 == 0) o_rdata1 = 0;
-		else o_rdata1 <= regs[i_raddr1];
-		
-		if(i_raddr2 == 0) o_rdata2 = 0;
-		else o_rdata2 <= regs[i_raddr2];
 	end
-  
+
+	assign o_rdata1 = i_raddr1 == 0 ? 0 : regs[i_raddr1];
+	assign o_rdata2 = i_raddr2 == 0 ? 0 : regs[i_raddr2];
+	
 endmodule
